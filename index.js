1bc190d3b0351c2dbd1a7dcc3456e988cf98c4bd
Prince Akpobasa <pakpobasa@gmail.com>
2025-12-18 16:13:32 +0100

    Initial commit: Basic Express server setup

---
+++ index.js
@@ -0,0 +1,34 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Prince E-commerce API!' });
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
